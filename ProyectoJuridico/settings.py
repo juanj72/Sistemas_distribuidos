2c7a3e09bf217a10278154541562e89d70e04dbb
Juan José Jara Álvarez <69679681+juanj72@users.noreply.github.com>
2022-10-11 14:40:47 -0500

    prueba 62 base de datos embebida

--- ProyectoJuridico/settings.py
+++ ProyectoJuridico/settings.py
@@ -16,9 +16,9 @@ from django.urls import reverse_lazy
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#STATICFILES_DIRS=(
- #   os.path.join(BASE_DIR,'static'),
-#)
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR,'static'),
+)
 
 
 # Quick-start development settings - unsuitable for production
